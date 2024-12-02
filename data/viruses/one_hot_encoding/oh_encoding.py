f07f66c62d7de5fbcf81ae64034ca944b6a7f795
Julia Qian <jqian310@gatech.edu>
2024-12-01 22:57:20 -0500

    cleaned up files

--- data/viruses/one_hot_encoding/oh_encoding.py
+++ data/viruses/one_hot_encoding/oh_encoding.py
@@ -10,23 +10,15 @@ def main():
     "release_other","cytoplasm","CE_clathrin","CE_receptor",
     "CE_glycoproteins","CE_other"]
 
-    # drop unneeded columns
-    # virus_traits_df = virus_traits_df.drop(["scientific name", "alternative name"], axis=1)
-
-    # Create an encoder
     encoder = OneHotEncoder(sparse_output=False)
 
-    # Apply encoder and make df of encoded columns
     one_hot_encoded = encoder.fit_transform(virus_traits_df[categorical_cols])
     one_hot_encoded_df = pd.DataFrame(one_hot_encoded, columns=encoder.get_feature_names_out(categorical_cols))
 
-    # Combine encoded columns and original
     processed_df = pd.concat([virus_traits_df, one_hot_encoded_df], axis=1)
 
-    # drop the unencoded categorical columns
     processed_df = processed_df.drop(categorical_cols, axis=1)
 
-    # Save the processed dataframe
     processed_df.to_csv("virus_traits_processed.csv", index=False)
 
 
